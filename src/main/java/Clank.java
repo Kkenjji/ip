170ff9235a59721a9c8b2d688667df352e12e1fe
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2025-01-18 16:36:42 +0800

    Add echoing functionality

--- src/main/java/Clank.java
+++ src/main/java/Clank.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Clank {
+
     public static void main(String[] args) {
-        String message = " Greetings! I'm Clank, Ratchet's best friend.\n" +
-                         " I heard you are in trouble, and I'm here to help.\n" +
-                         " How may I assist you?\n" +
-                         "____________________________________________________________\n" +
-                         " Bye. Hope to see you again soon!\n";
-        System.out.println(message);
+        String line = "____________________________________________________________\n";
+        String welcomeMessage = " Greetings! I'm Clank, Ratchet's best friend.\n" +
+                                " I heard you are in trouble, and I'm here to help.\n" +
+                                " How may I assist you?\n" +
+                                line;
+        String byeMessage = " Bye. Hope to see you again soon!\n";
+
+        System.out.println(welcomeMessage);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            System.out.println(line);
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(byeMessage);
+                break;
+            }
+            System.out.println(input);
+
+            System.out.println(line);
+        }
     }
 }
