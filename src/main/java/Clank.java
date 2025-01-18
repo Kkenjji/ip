7de356dcf6c4fa241efe08447016b83c1280a603
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2025-01-18 16:40:36 +0800

    Add ability to store and list out texts

--- src/main/java/Clank.java
+++ src/main/java/Clank.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Clank {
+    private static ArrayList<String> messages = new ArrayList<>();
 
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
         String welcomeMessage = " Greetings! I'm Clank, Ratchet's best friend.\n" +
-                                " I heard you are in trouble, and I'm here to help.\n" +
-                                " How may I assist you?\n" +
-                                line;
-        String byeMessage = " Bye. Hope to see you again soon!\n";
+                " I heard you are in trouble, and I'm here to help.\n" +
+                " How may I assist you?\n" +
+                line;
 
+        String byeMessage = " Bye. Hope to see you again soon!\n";
         System.out.println(welcomeMessage);
 
         Scanner scanner = new Scanner(System.in);
@@ -23,8 +25,15 @@ public class Clank {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(byeMessage);
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < messages.size(); i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". " + messages.get(i));
+                }
+            } else {
+                messages.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
 
             System.out.println(line);
         }
