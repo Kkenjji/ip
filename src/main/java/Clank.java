4ea75d2ba41829bdccc06eb43d1aea59e0dfa6f7
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2025-01-18 10:48:31 +0800

    Rename Duke to Clank

---
+++ src/main/java/Clank.java
@@ -0,0 +1,10 @@
+public class Clank {
+    public static void main(String[] args) {
+        String message = " Greetings! I'm Clank, Ratchet's best friend.\n" +
+                         " I heard you are in trouble, and I'm here to help.\n" +
+                         " How may I assist you?\n" +
+                         "____________________________________________________________\n" +
+                         " Bye. Hope to see you again soon!\n";
+        System.out.println(message);
+    }
+}
