bd9f0287efef43244896a67f9913db3ae1b6f097
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2025-02-02 13:57:21 +0800

    Rename constants

--- src/main/java/Clank.java
+++ src/main/java/Clank.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Clank {
-    private static TaskManager taskManager = new TaskManager();
+    private static final String LINE = "____________________________________________________________\n";
+    private static final String WELCOME_MESSAGE = " Greetings! I'm Clank, Ratchet's best friend.\n" +
+            " I heard you are in trouble, and I'm here to help.\n" +
+            " How may I assist you?\n" +
+            LINE;
+    private static final String BYE_MESSAGE = " Bye. Hope to see you again soon!\n";
+    private static final TaskManager taskManager = new TaskManager();
 
     public static void main(String[] args) {
-        String line = "____________________________________________________________\n";
-        String welcomeMessage = " Greetings! I'm Clank, Ratchet's best friend.\n" +
-                " I heard you are in trouble, and I'm here to help.\n" +
-                " How may I assist you?\n" +
-                line;
-
-        String byeMessage = " Bye. Hope to see you again soon!\n";
-        System.out.println(welcomeMessage);
+        System.out.println(WELCOME_MESSAGE);
 
         Scanner scanner = new Scanner(System.in);
         String input;
@@ -25,12 +23,12 @@ public class Clank {
             String first = splitted[0];
             String second = (splitted.length == 2) ? splitted[1] : "";
 
-            System.out.println(line);
+            System.out.println(LINE);
 
             try {
                 switch (first) {
                 case "bye":
-                    System.out.println(byeMessage);
+                    System.out.println(BYE_MESSAGE);
                     System.exit(0);
                     break;
                 case "list":
@@ -63,7 +61,7 @@ public class Clank {
                 System.out.println("An unexpected error occured: " + e.getMessage());
             }
 
-            System.out.println(line);
+            System.out.println(LINE);
         }
     }
 }
