06117db5b16e7fdcd70b0a4cae76c96cbf0377cc
Kkenjji <156585410+Kkenjji@users.noreply.github.com>
2025-02-08 01:17:48 +0800

    Add Javadocs for test files

--- src/test/java/clank/command/AddCommandTest.java
+++ src/test/java/clank/command/AddCommandTest.java
@@ -6,13 +6,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Unit tests for the {@code AddCommand} class.
+ */
 public class AddCommandTest {
+
+    /**
+     * Tests the creation of a Todo task using AddCommand.
+     */
     @Test
     public void testAddTodoCommand() {
         AddCommand command = new AddCommand("todo Read book");
         assertNotNull(command);
     }
 
+    /**
+     * Tests that an invalid AddCommand input throws a {@code ClankException}.
+     */
     @Test
     public void testInvalidAddCommand() {
         assertThrows(ClankException.class, () -> new AddCommand("unknown task"));
